35104d7573bffdb38a647167d31fe22f1cf04070
bazinga <lgl050712@163.com>
2022-04-15 17:58:09 +0800

    init

---
+++ src/main/java/org/elastic/toy/db/resp/RespInitializerHandler.java
@@ -0,0 +1,15 @@
+package org.elastic.toy.db.resp;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+/**
+ * @author bazinga
+ * 2022-4-15 15:11:42
+ */
+public class RespInitializerHandler extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+    }
+}
--- src/main/java/org/elastic/toy/db/server/RedisServer.java
+++ src/main/java/org/elastic/toy/db/server/RedisServer.java
@@ -1,9 +1,15 @@
 package org.elastic.toy.db.server;
 
 import com.google.common.base.Strings;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.elastic.toy.db.config.RedisConfig;
+import org.elastic.toy.db.resp.RespInitializerHandler;
 
 public class RedisServer {
 
@@ -25,6 +31,25 @@ public class RedisServer {
     public void start() {
 
         checkConfig();
+        try{
+            bossGroup = new NioEventLoopGroup(1);
+            workerGroup = new NioEventLoopGroup(Runtime.getRuntime().availableProcessors() * 2);
+
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup,workerGroup).channel(NioServerSocketChannel.class).
+                    childHandler(new RespInitializerHandler()).
+                    option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT).
+                    childOption(ChannelOption.SO_RCVBUF,BUFFER_SIZE).
+                    childOption(ChannelOption.SO_SNDBUF,BUFFER_SIZE).
+                    childOption(ChannelOption.SO_KEEPALIVE,true).
+                    childOption(ChannelOption.ALLOCATOR,PooledByteBufAllocator.DEFAULT);
+
+            future = bootstrap.bind(redisConfig.getHost(), redisConfig.getPort());
+
+            future.channel().closeFuture().sync();
+        }catch (Exception e){
+
+        }
     }
 
     private void checkConfig() {
